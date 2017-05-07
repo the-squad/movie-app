1a3725e7ce4adeba6f1c477dc0b6be3241edeb9c
MostafaMagdy <Megz20140415github>
2017-05-07 21:43:10 +0200

    reset_password

---
+++ app/src/main/java/com/example/android/movieapp/Reset_password.java
@@ -0,0 +1,21 @@
+package com.example.android.movieapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Reset_password extends AppCompatActivity {
+
+    EditText email_text;
+    Button send_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+
+
+    }
+}
